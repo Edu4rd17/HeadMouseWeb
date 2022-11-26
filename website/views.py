5262eccad7b1b4a5a4273fe0b7321761c01d904a
Eduard Iacob <B00125295@mytudublin.ie>
2022-11-26 00:41:30 +0000

    authentication system added and some layouts

--- website/views.py
+++ website/views.py
@@ -1,7 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask_login import login_user, login_required, logout_user, current_user
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
+@login_required
 def home():
-    return "<h1>Home</h1>"
+    return render_template("home.html", user=current_user)
+
+
