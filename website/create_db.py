6f6f0864df0eb23c5693974524378626274b93ca
Eduard Iacob <B00125295@mytudublin.ie>
2022-12-06 13:31:33 +0000

    code reformatting

--- website/create_db.py
+++ website/create_db.py
@@ -5,12 +5,11 @@ import os
 load_dotenv()
 
 mydb = mysql.connector.connect(
-    host= os.getenv('LOCALHOST'),
-    user="root",
-    password="Database2001",
+    host=os.getenv('LOCALHOST'),
+    user=os.getenv('LOCALHOST_USER'),
+    password=os.getenv('LOCALHOST_PASS'),
 )
 
 my_cursor = mydb.cursor()
 
 # my_cursor.execute("CREATE DATABASE headmouseweb")
-
