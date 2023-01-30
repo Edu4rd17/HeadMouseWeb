b6f9a7911a1f3cd38a8f81ea35e2bb1931dcd023
Eduard Iacob <B00125295@mytudublin.ie>
2023-01-30 17:46:38 +0000

    code formatting

--- website/views.py
+++ website/views.py
@@ -8,6 +8,7 @@ import pyautogui
 views = Blueprint('views', __name__)
 
 camera = cv2.VideoCapture(0)
+# the refine landmarks is used to get more accurate landmarks on the face
 face_mesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)
 screen_width, screen_height = pyautogui.size()
 
@@ -20,20 +21,21 @@ def gen_frames():  # generate frame by frame from camera
         frame = cv2.flip(frame, 1)
         # convert the frame to RGB
         rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        # process the frame using face_mesh
+        # process the rgb_frame using face_mesh
         results = face_mesh.process(rgb_frame)
-        # landmarks of the face
+        # landmarks of the face at this point if we print this the system is able to tell if there s a face present or not
         landmark_points = results.multi_face_landmarks
-        # draw the landmarks on the frame
+        # get the height and width of the frame
         frame_height, frame_width, _ = frame.shape
+        # check if there are landmarks
         if landmark_points:
-            # loop through all the landmark points
             landmarks = landmark_points[0].landmark
+            # loop through all the landmark points; select a range of index as we are only interested in the eyes
             for id, landmark in enumerate(landmarks[474:478]):
-                # get the x and y coordinates of the landmarks
+                # get the x and y coordinates of the landmarks, to draw the circle we need to cast to a integer number (its required)
                 x = int(landmark.x * frame_width)
                 y = int(landmark.y * frame_height)
-                # draw a circle on the landmark
+                # draw a circle on the landmark; x and y are the center ; 3 is the radius and 0, 255, 0 is the color
                 cv2.circle(frame, (x, y), 3, (0, 255, 0))
                 if id == 1:
                     screen_x = screen_width / frame_width * x
@@ -44,6 +46,7 @@ def gen_frames():  # generate frame by frame from camera
                 x = int(landmark.x * frame_width)
                 y = int(landmark.y * frame_height)
                 cv2.circle(frame, (x, y), 3, (0, 255, 255))
+            # check if the distance between the two points is less than 0.004
             if (left[0].y - left[1].y) < 0.004:
                 pyautogui.click()
                 pyautogui.sleep(1)
@@ -51,6 +54,7 @@ def gen_frames():  # generate frame by frame from camera
         frame = buffer.tobytes()
         # concat frame one by one and show result
         yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
 
 @ views.route('/')
 def index():
@@ -72,6 +76,7 @@ def home():
 @login_required
 def youtube():
     return render_template("youtube.html", user=current_user)
+
 
 @ views.route('/about')
 def about():
