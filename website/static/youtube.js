8e283388fe06c2d5a5dcef4d2323542156ff831b
Eduard Iacob <B00125295@mytudublin.ie>
2023-02-24 22:13:16 +0000

    updated Youtube functionalities

--- website/static/youtube.js
+++ website/static/youtube.js
@@ -1,92 +1,32 @@
-$(document).ready(function () {
-  var YT_API_KEY = "";
-  var URL = "https://www.googleapis.com/youtube/v3/search?key=";
-  var maxNumResults = 20;
-
-  // $("#form")[0].reset();
-  $("#form").submit(function (event) {
-    // Prevent the form from submitting via the browser.
-    event.preventDefault();
-    // Get the search term
-    var search = $("#search").val();
-    if (document.getElementById("search").value.trim().length > 0) {
-      videoSearch(YT_API_KEY, search, maxNumResults);
-    } else {
-      alert("Please enter a search term");
-    }
-  });
-
-  function videoSearch(key, search, maxResults) {
-    $.get(
-      URL +
-        key +
-        "&type=video&part=snippet&maxResults=" +
-        maxResults +
-        "&q=" +
-        search,
-      function (data) {
-        console.log(data);
-        var id = data.items[0].id.videoId;
-        mainVideo(id);
-        resultsLoop(data);
-      }
-    );
-  }
-
-  // function mainVideo(id) {
-  //   $("#video").html(
-  //     `<iframe height="400" width="900" src="http://www.youtube.com/embed/${id}?autoplay=0&showinfo=0&controls=0" frameborder="0" allowfullscreen></iframe>`
-  //   );
-  // }
-
-  function resultsLoop(data) {
-    $.each(data.items, function (i, item) {
-      var thumb = item.snippet.thumbnails.medium.url;
-      var title = item.snippet.title;
-      var description = item.snippet.description.substring(0, 100);
-      var vid = item.id.videoId;
-
-      $("main").append(`
-      <article class="item" data-key="${vid}">
-        <img src="${thumb}" alt="" class="thumb" />
-        <div class="details">
-          <h4 class="video-title">${title}</h4>
-          <p class="video-description">${description}</p>
-        </div>
-      </article>
-    `);
-    });
-  }
-
-  $("main").on("click", "article", function () {
-    var id = $(this).attr("data-key");
-    console.log(id);
-    videoDuration(id);
-    playSelectedVideo(id);
-  });
-});
-
 var tag = document.createElement("script");
 
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName("script")[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
 var player;
-function mainVideo(id) {
-  player = new YT.Player("video", {
-    height: "500",
-    width: "1200",
-    videoId: id,
-    autoplay: 1,
-    playerVars: {
-      controls: 0,
-      showinfo: 0,
-      rel: 0,
-      modestbranding: 1,
-      order: "date",
-      // enablejsapi: 1
-    },
+function onYouTubeIframeAPIReady() {
+  // player = ("player", {
+  //   height: "500",
+  //   width: "1200",
+  //   videoId: `${id}`,
+  //   host: "https://www.youtube.com",
+  //   autoplay: 1,
+  //   playerVars: {
+  //     controls: 0,
+  //     showinfo: 0,
+  //     rel: 0,
+  //     modestbranding: 1,
+  //     order: "date",
+  //     enablejsapi: 1,
+  //     origin: "http://127.0.0.1:5000",
+  //   },
+  //   events: {
+  //     onReady: onPlayerReady,
+  //     onStateChange: onPlayerStateChange,
+  //   },
+  // });
+
+  player = new YT.Player("existing-iframe-example", {
     events: {
       onReady: onPlayerReady,
       onStateChange: onPlayerStateChange,
@@ -94,24 +34,42 @@ function mainVideo(id) {
   });
 }
 
-function playSelectedVideo(id) {
-  player.loadVideoById(id);
-  // player.playVideo();
-}
-
 function onPlayerReady(event) {
+  document.getElementById("existing-iframe-example").style.borderColor =
+    "#FF6D00";
   event.target.playVideo();
-  videoDuration();
+  // videoDuration();
   // liveVideoTime();
 }
 
-// function playNextVideo() {
-//   var seconds;
-//   if (seconds == player.getDuration()) {
-//     player.nextVideo();
-//     player.playVideo();
-//   }
-// }
+function changeBorderColor(playerStatus) {
+  var color;
+  if (playerStatus == -1) {
+    color = "#37474F"; // unstarted = gray
+  } else if (playerStatus == 0) {
+    color = "#FFFF00"; // ended = yellow
+  } else if (playerStatus == 1) {
+    color = "#33691E"; // playing = green
+  } else if (playerStatus == 2) {
+    color = "#DD2C00"; // paused = red
+  } else if (playerStatus == 3) {
+    color = "#AA00FF"; // buffering = purple
+  } else if (playerStatus == 5) {
+    color = "#FF6DOO"; // video cued = orange
+  }
+  if (color) {
+    document.getElementById("existing-iframe-example").style.borderColor =
+      color;
+  }
+}
+
+
+function playSelectedVideo(id) {
+  player.loadVideoById(id);
+
+  return false;
+  // player.playVideo();
+}
 
 function liveVideoTime() {
   var liveVideoTime;
@@ -120,10 +78,10 @@ function liveVideoTime() {
 }
 
 function videoDuration(id) {
-  var videoDuration = 0;
+  var videoDuration;
   videoDuration = formatTime(player.getDuration(id));
   $("#videoDuration").html(`<p>Video Duration: ${videoDuration}</p>`);
-  // console.log(videoDuration);
+  console.log(videoDuration);
 }
 
 function formatTime(time) {
@@ -138,6 +96,8 @@ function formatTime(time) {
 }
 
 function onPlayerStateChange(event) {
+  changeBorderColor(event.data);
+
   if (player.getPlayerState() === 1 || player.getPlayerState() === 2) {
     //forward the video by 10 seconds
     $("#fwrd").prop("disabled", false);
@@ -239,6 +199,8 @@ function forwardTenSec() {
 
 function pauseVideo() {
   player.pauseVideo();
+  console.log("paused video");
+  // alert("paused video");
 }
 
 function playVideo() {
@@ -281,6 +243,90 @@ function normalSpeedVideo() {
   var currSpeed = player.getPlaybackRate();
   player.setPlaybackRate((currSpeed = 1));
 }
+
+// function mainVideo(id) {
+//   $("#playerr").html(
+//     `<iframe height="500" width="1200" id="player" src="https://www.youtube.com/embed/${id}?autoplay=1&showinfo=0&controls=0&enablejsapi=1" allow="autoplay" frameborder="0" allowfullscreen></iframe>`
+//   );
+// }
+
+function mainVideo(id) {
+  document.getElementById(
+    "existing-iframe-example"
+  ).src = `https://www.youtube.com/embed/${id}?autoplay=1&showinfo=0&controls=0&enablejsapi=1&origin:http://127.0.0.1:5000`;
+}
+
+$(document).ready(function () {
+  var YT_API_KEY = "";
+  var URL = "https://www.googleapis.com/youtube/v3/search?key=";
+  var maxNumResults = 20;
+
+  $("#form").submit(function (event) {
+    // Prevent the form from submitting via the browser.
+    event.preventDefault();
+    // Get the search term
+    var search = $("#search").val();
+    if (document.getElementById("search").value.trim().length > 0) {
+      videoSearch(YT_API_KEY, search, maxNumResults);
+    } else {
+      alert("Please enter a search term");
+    }
+  });
+
+  function videoSearch(key, search, maxResults) {
+    $.get(
+      URL +
+        key +
+        "&part=snippet&maxResults=" +
+        maxResults +
+        "&type=video&q=" +
+        search,
+      function (data) {
+        console.log(data);
+        //empty data from previous search
+        //empty the main video function
+        var id = data.items[0].id.videoId;
+        // alert(player);
+        // alert(id);
+        $("#player").empty();
+        mainVideo(id);
+        // onYouTubeIframeAPIReady();
+        $("main").empty();
+        resultsLoop(data);
+        // videoDuration(id);
+      }
+    );
+  }
+
+  function resultsLoop(data) {
+    // clear
+
+    $.each(data.items, function (i, item) {
+      var thumb = item.snippet.thumbnails.medium.url;
+      var title = item.snippet.title;
+      var description = item.snippet.description.substring(0, 100);
+      var vid = item.id.videoId;
+
+      $("main").append(`
+      <article class="item" data-key="${vid}">
+        <img src="${thumb}" alt="" class="thumb" />
+        <div class="details">
+          <h4 class="video-title">${title}</h4>
+          <p class="video-description">${description}</p>
+        </div>
+      </article>
+    `);
+    });
+  }
+
+  $("main").on("click", "article", function () {
+    var id = $(this).attr("data-key");
+    console.log(id);
+    // videoDuration(id);
+    // playSelectedVideo(id);
+    mainVideo(id);
+  });
+});
 
 // $(document).ready(function () {
 //   var YT_API_KEY = "";
